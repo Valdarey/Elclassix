ea512ab7dae8edd57a0c8fda5e1b95d9c9dd71f5
Valdarey <prabaswaradaffa2@gail.com>
2025-02-28 23:49:48 +0700

    first commit

---
+++ skrip.js
@@ -0,0 +1,33 @@
+// Script biar responsive hamburger-menu
+const menu = document.querySelector ('.menu-nav');
+document.querySelector('#hamburger-menu').onclick = () => {
+    menu.classList.toggle('active');
+};
+
+const hamburger = document.querySelector('#hamburger-menu'); 
+document.addEventListener('click', function (hapus) {
+    if(!hamburger.contains(hapus.target) && !menu.contains(hapus.target)) {
+        menu.classList.remove('active');
+    }
+});
+
+// Script biar responsive medsos
+const medsos = document.querySelector ('.medsos');
+document.querySelector('#medsos-nav').onclick = () => {
+    medsos.classList.toggle('active');
+};
+
+const mediaSosial = document.querySelector('#medsos-nav');
+document.addEventListener('click', function (hapus) {
+    if(!mediaSosial.contains(hapus.target)) {
+        medsos.classList.remove('active');
+    }
+});
+
+// Script Type biar loop dan jalan
+var typed = new Typed('.auto-type', {
+    strings: ['SELAMAT DATANG', 'BAGAIMANA KABARMU?', 'LUPAKAH KALIAN?','GRUP SEPI KALI BRAY', 'SEMOGA SEHAT SEMUA',
+        ''],
+    typeSpeed: 50,
+    loop:true
+});
